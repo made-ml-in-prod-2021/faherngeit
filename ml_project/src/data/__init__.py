dd0fd0e21d3c5f66d5c31015f81e0f0ca69d9155
faherngeit <valeriy.tashchilin@hey.com>
2021-06-20 14:25:50 +0500

    Remove first homework files

--- ml_project/src/data/__init__.py
+++
@@ -1,7 +0,0 @@
-from .make_dataset import read_dataset, generate_random_dataset, generate_random_numerical, \
-    generate_random_categorical, CAT_FEATURE_PREPROCESSING
-
-
-__all__ = ["read_dataset", "generate_random_dataset", "generate_random_categorical",
-           "generate_random_numerical",
-           "CAT_FEATURE_PREPROCESSING"]
--- ml_project/src/features/__init__.py
+++
@@ -1,3 +0,0 @@
-from .build_features import process_categorical_data, prepare_data_to_train
-
-__all__ = ["process_categorical_data", "prepare_data_to_train"]
